4b941e47961e11cf1c604ac4807d3522ebd67f59
reski-id <pesan.reski@gmail.com>
2022-08-04 02:28:08 +0700

    reguest respons:

---
+++ feature/cart/delivery/request.go
@@ -0,0 +1,22 @@
+package delivery
+
+import (
+	"commerce-app/domain"
+	"time"
+)
+
+type CartInsertRequest struct {
+	Quantity  int  `json:"qty" form:"qty" validate:"required"`
+	UserID    uint `json:"user_id" form:"user_id"`
+	ProductID uint `json:"product_id" form:"product_id"`
+}
+
+func (ci *CartInsertRequest) ToDomain() domain.Cart {
+	return domain.Cart{
+		Quantity:  ci.Quantity,
+		ProductID: int(ci.ProductID),
+		UserID:    int(ci.UserID),
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+	}
+}
