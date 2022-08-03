b9f92245d7da84d3143e9153decaa7fa5dff4f64
reski-id <pesan.reski@gmail.com>
2022-08-04 01:30:25 +0700

    cart domain usecase usedata handler

---
+++ domain/cart.go
@@ -0,0 +1,40 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Cart struct {
+	ID        int
+	Quantity  int
+	ProductID int
+	UserID    int
+	CreatedAt time.Time
+	UpdatedAt time.Time
+}
+
+type CartUseCase interface {
+	AddCart(newCart Cart) (Cart, error)
+	GetAllC() ([]Cart, error)
+	UpCart(IDCart int, updateData Cart) (Cart, error)
+	DelCart(IDCart int) (bool, error)
+	GetSpecificCart(cartID int) ([]Cart, error)
+}
+
+type CartHandler interface {
+	InsertCart() echo.HandlerFunc
+	GetAllCart() echo.HandlerFunc
+	UpdateCart() echo.HandlerFunc
+	DeleteCart() echo.HandlerFunc
+	GetCartID() echo.HandlerFunc
+}
+
+type CartData interface {
+	Insert(insertCart Cart) Cart
+	GetAll() []Cart
+	Update(IDCart int, updatedCart Cart) Cart
+	Delete(IDCart int) bool
+	GetCartID(cartID int) []Cart
+}
